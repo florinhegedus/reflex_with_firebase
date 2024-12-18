7ef5b35bb543f7ee02b8d87cda0e84d84bfe8450
florinhegedus <florinxhgds@gmail.com>
2024-12-18 11:37:01 +0200

    remove loginstate and registerstate

--- reflex_with_firebase/pages/login.py
+++ reflex_with_firebase/pages/login.py
@@ -2,16 +2,6 @@ import reflex as rx
 from ..firebase import FirebaseState
 
 
-class LoginState(rx.State):
-    form_data: dict = {}
-
-    @rx.event
-    def handle_submit(self, form_data: dict):
-        """Handle the form submit."""
-        self.form_data = form_data
-        FirebaseState.login(self.form_data)
-
-
 def login_form() -> rx.Component:
     return rx.vstack(
         rx.heading("Login"),
--- reflex_with_firebase/pages/register.py
+++ reflex_with_firebase/pages/register.py
@@ -1,17 +1,5 @@
 import reflex as rx
 from ..firebase import FirebaseState
-import json
-
-
-class RegisterState(rx.State):
-    form_data: dict = {}
-    message: str = "NOT SET"
-
-    @rx.event
-    def handle_submit(self, form_data: dict):
-        """Handle the form submit."""
-        self.form_data = form_data
-        FirebaseState.register(self.form_data)
 
 
 def register_form():
